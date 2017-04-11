952629b100cc9c16695b5f15e03fadc35c118f62
Justin-Byrne <justin@byrne-systems.com>
2017-04-10 22:41:10 -0700

    Refined view hierarchy; included some new mock images; adjusted less-pure grid system

--- inc/controller.php
+++ inc/controller.php
@@ -18,51 +18,73 @@ require '/../lib/_Framework/template.class.php';
 # Set: Root Directory
 $root = rtrim(dirname(__FILE__), '/inc');
 
-// session_start();                                        # Session: start
+session_start();                                        # Session: start
 
-# ------------------------------------ [ Header ] ------------------------------------ #
+# ------------------------------------ [ Header ] --------------------------- #
 
 # Load: ancillary (or contributory) views
-$notes  = new Template($root . '/web/views/' . 'notes.wad');
-$meta   = new Template($root . '/web/views/' . 'meta.wad');
-$styles = new Template($root . '/web/views/' . 'style-sheets.wad');
-$script = new Template($root . '/web/views/' . 'script.wad');
+$notes  = new Template($root . '/web/views/meta/' . 'notes.wad');
+$meta   = new Template($root . '/web/views/meta/' . 'meta.wad');
+$styles = new Template($root . '/web/views/meta/' . 'style-sheets.wad');
+$script = new Template($root . '/web/views/meta/' . 'script.wad');
 
-####################################################################
-###                       Load Template(s)                       ###
-####################################################################
-
-# Load: layout 'bootstrap'
-$layout = new Template($root . '/web/views/layouts/'  . 'html5.wad');
-
-# Load: primary page elements
-$header = new Template($root . '/web/views/' . 'header.wad');
-$main   = new Template($root . '/web/views/' . 'main.wad');
-$footer = new Template($root . '/web/views/' . 'footer.wad');
+###############################################################################
+###                       Load Template(s)                                   ##
+###############################################################################
 
 # Load: composite application template
 $app    = new Template($root . '/web/views/' . 'app.wad');
 
-# Load: project and/or application frame(s)
-// Cycle through various projects applications
+# Load: layout 'bootstrap'
+$layout = new Template($root . '/web/views/layouts/'  . 'html5.wad');
 
-####################################################################
-###                       Build Master Page                      ###
-####################################################################
+$_SESSION['Page'] = 'blog';
+
+switch ($_SESSION['Page']) {
+  case 'blog':
+    # Load: blog page page elements
+    $blog    = new Template($root . '/web/views/pages/blog/' . 'blog.wad');
+    $sidebar = new Template($root . '/web/views/pages/blog/' . 'sidebar.wad');
+    $content = new Template($root . '/web/views/pages/blog/' . 'content.wad');
+
+    ############################################################################
+    ###                       Setup Landing Page                             ###
+    ############################################################################
+    $layout->set('header', '');
+    $layout->set('favicon',                 $sidebar->favicon($root . '/web/images/fav/cube.fav'));
+    $blog->set('sidebar',                   $sidebar->output());
+    $blog->set('content',                   $content->output());
+    $layout->set('main',                    $blog->output());
+    $layout->set('footer',                  '');
+    break;
+  default:
+    # Load: landing page elements
+    $header = new Template($root . '/web/views/pages/landing/' . 'header.wad');
+    $main   = new Template($root . '/web/views/pages/landing/' . 'main.wad');
+    $footer = new Template($root . '/web/views/pages/landing/' . 'footer.wad');
+
+    ############################################################################
+    ###                       Setup Landing Page                             ###
+    ############################################################################
+    $layout->set('header',                  $header->output());
+    $layout->set('favicon',                 $header->favicon($root . '/web/images/fav/cube.fav'));
+    $layout->set('main',                    $main->output());
+    $layout->set('footer',                  $footer->output());
+    break;
+}
+
+###############################################################################
+###                       Finalize Page with Meta Data                      ###
+###############################################################################
 
 # Set: tags inside layout 'bootstrap'
 $layout->set('file_notes',              $notes->output());
 $layout->set('title',                   'Byrne-Systems | Web-Application Design & Development');
 $layout->set('meta_tags',               $meta->output());
-$layout->set('favicon',                 $header->favicon($root . '/web/images/fav/cube.fav'));
+// $layout->set('favicon',                 $header->favicon($root . '/web/images/fav/cube.fav'));
 $layout->set('styles',                  $styles->output());
 $layout->set('wrapper',                 'Byrne-Systems');
-
-$layout->set('header',                  $header->output());
-$layout->set('main',                    $main->output());
-$layout->set('footer',                  $footer->output());
-
 $layout->set('script',                  $script->output());
 
 # Compile: all pre-parsed views into a single application view 'app.wad' to echo (or print) to users' screen; or stdout
-$app->set('app',                            $layout->output());
+$app->set('app',                        $layout->output());
